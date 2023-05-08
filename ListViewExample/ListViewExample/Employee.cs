3a249991826dea91f999f0099147f541a722f3f2
diegoporras <diegoporras@gmail.com>
2023-05-07 20:58:28 -0500

    Agregar archivos de proyecto.

---
+++ ListViewExample/ListViewExample/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ListViewExample
+{
+    public class Employee 
+    {
+       public string DisplayName { get; set; }
+    }
+}
